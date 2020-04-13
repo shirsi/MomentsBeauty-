8d03bd0c0e881fbb1e6a9ad0a4a0ebbe34bf9e1c
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-04-13 10:11:26 -0500

    home

--- mbeauty/urls.py
+++ mbeauty/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from .views import index
 from django.conf.urls.static import static
@@ -23,7 +23,5 @@ urlpatterns = [
     path('', include('shop.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-    re_path(r'^.*$', index)
-
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
